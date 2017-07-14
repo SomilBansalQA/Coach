f0cef7d754354d8018996f3fc6de3616c71e9e5e
SomilBansalQA <somilbansal@qainfotech.com>
2017-07-14 11:13:06 +0530

    Update TestCoach.java

--- src/test/java/org/qait/coach/coach1/TestCoach.java
+++ src/test/java/org/qait/coach/coach1/TestCoach.java
@@ -1,71 +1,64 @@
-﻿package org.qait.coach.coach1;
+package org.qait.coach.coach1;
 
-import static org.testng.Assert.assertTrue;
 import static org.testng.AssertJUnit.assertEquals;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 class TestCoach {
-	WebDriver driver;
-
+     
 	Coach_Gift_Card coachGCObj;
 	ExcelFile efObj;
-    int sleepTime=5000;
-	
+    Driver driverobj;
+	int sleepTime=5000;
+	WebDriver driver;
+	String GiftcardTitle="SEND STYLE";
+	String giftCardDescription="The luxury of choice is a wonderful gift.";
+	String giftCard="GIFT CARDS";
 	@BeforeTest
 	public void setup() {
-		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.get("https://storefront:coach123@dwdevelopment2.coach.com/");
+	    driverobj = new Driver();
+		driver= driverobj.driverLaunch();		
 		coachGCObj = new Coach_Gift_Card(driver);
 		efObj = new ExcelFile();
 	}
 
+	
+	
 	@Test(priority = 0)
 	public void TestGiftCards() throws InterruptedException {
 		
 		
 		coachGCObj.clickOnGiftCards();
 		coachGCObj.closePopWindow();
-	
-		Thread.sleep(sleepTime);
-		((JavascriptExecutor) driver).executeScript("document.getElementById('search-result-content').scrollIntoView(true);");
-		Thread.sleep(sleepTime); 
+	    
+		coachGCObj.scrollIntoView();
 		
-		assertEquals("SEND STYLE", driver.findElement( By.xpath(".//h1[contains(text(),'SEND STYLE')]")).getText());
-		assertEquals("The luxury of choice is a wonderful gift.", driver.findElement( By.xpath(".//div[contains(text(),'The luxury of choice is a wonderful gift.')]")).getText());	
-		assertEquals("GIFT CARDS", driver.findElement( By.xpath(".//div[contains(text(),'GIFT CARDS')]")).getText());
+	assertEquals("check GiftcardTitle",GiftcardTitle, coachGCObj.getGiftCardTitle());
+	assertEquals("check giftCardDescription",giftCardDescription, coachGCObj.getGiftCardDescription());	
+	assertEquals("check giftCardText",giftCard, coachGCObj.getGiftCardText());
 
 			
 	    int	cursor=1;
 	    while(cursor<efObj.getNoOfRows()-2){
-	    Thread.sleep(sleepTime);
+	 
 		coachGCObj.clickOnCheckYourBalance();
-		Thread.sleep(sleepTime);
-		
+	
 		coachGCObj.setGiftCardNumber(efObj.getGiftCardNumber(cursor));
-		Thread.sleep(sleepTime);
 
 		coachGCObj.setGiftCardPinNumber(efObj.getGiftCardPinNumber(cursor));
-		
-		Thread.sleep(sleepTime);
 		System.out.println("Gift Card No: is " +efObj.getGiftCardNumber(cursor) + "Pin No: " + efObj.getGiftCardPinNumber(cursor));
+
 		coachGCObj.clickOnSubmitButton();
-		Thread.sleep(sleepTime);
-        
-		String Balance=driver.findElement(By.xpath(".//div[@class='row balance-row']/span")).getText();
-		
+	
+
+		String Balance=coachGCObj.getGiftCardBalance();
+
  	    System.out.println("Balance is "+Balance);
 		
 		coachGCObj.closePopWindow();
-		Thread.sleep(sleepTime);
+
  	    cursor++;
 
 	}
